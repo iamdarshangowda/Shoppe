179cf68b7800264c4f2319167e4adf534fa8c63c
iamdarshangowda <iamdarshangowda@gmail.com>
2022-12-05 00:54:20 +0530

    ui updated

--- src/components/layouts/layout.tsx
+++ src/components/layouts/layout.tsx
@@ -11,7 +11,14 @@ export const Layout: React.FunctionComponent<Props> = ({ children }) => {
   const router = useRouter();
   return (
     <Container maxWidth="xl">
-      <Box>{router.pathname == "/" ? null : <Topbar />}</Box>
+      <Box>
+        {router.pathname == "/" ||
+        router.pathname == "/signin" ||
+        router.pathname == "/signup" ||
+        router.pathname == "/forgotpassword" ? null : (
+          <Topbar />
+        )}
+      </Box>
       <Box component="main" width="100%" mt={3}>
         {children}
       </Box>
